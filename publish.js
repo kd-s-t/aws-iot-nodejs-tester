1b5a824e8f1a9c29fc049b632999d634afdb192c
Ollololl Ollooloo <ollololl.ollooloo@gmail.com>
2021-03-06 15:28:56 +0800

    gitignore added

--- publish.js
+++ publish.js
@@ -2,13 +2,13 @@ const awsIot = require('aws-iot-device-sdk');
 const path = require('path');
 
 let device = awsIot.device({
-  keyPath: path.resolve(__dirname, './certs/6c48f6ccab-private.pem.key'),
- certPath: path.resolve(__dirname, './certs/6c48f6ccab-certificate.pem.crt'),
+  keyPath: path.resolve(__dirname, './certs/06f142bc14-private.pem.key'),
+ certPath: path.resolve(__dirname, './certs/06f142bc14-certificate.pem.crt'),
    caPath: path.resolve(__dirname, './certs/AmazonRootCA1.pem'),
- clientId: 'test5',
+ clientId: 'test6',
      host: 'a2b0xwckvjuafw-ats.iot.us-east-2.amazonaws.com' 
 });
  
 device.on('connect', function() {
-    device.publish('test', JSON.stringify({ message: "test5 iot is connected"}));
+    device.publish('channel-test', JSON.stringify({ message: "i have come in peace"}));
 });
--- subscribe.js
+++ subscribe.js
@@ -2,16 +2,16 @@ const awsIot = require('aws-iot-device-sdk');
 const path = require('path');
 
 let device = awsIot.device({
-  keyPath: path.resolve(__dirname, './certs/6c48f6ccab-private.pem.key'),
- certPath: path.resolve(__dirname, './certs/6c48f6ccab-certificate.pem.crt'),
+  keyPath: path.resolve(__dirname, './certs/06f142bc14-private.pem.key'),
+ certPath: path.resolve(__dirname, './certs/06f142bc14-certificate.pem.crt'),
    caPath: path.resolve(__dirname, './certs/AmazonRootCA1.pem'),
- clientId: 'test5',
-     host: 'a2b0xwckvjuafw-ats.iot.us-east-2.amazonaws.com',
+ clientId: 'test6',
+     host: 'a2b0xwckvjuafw-ats.iot.us-east-2.amazonaws.com', 
 autoResubscribe: true
 });
  
 device.on('connect', function() {
-    device.subscribe('test');
+    device.subscribe('channel-test');
 });
  
 device.on('message', function(topic, payload) {
